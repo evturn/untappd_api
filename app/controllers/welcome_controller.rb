d4f4f7e28614e0a5d6c3b02e0e7d9fbcb2e44b2c
Evan <evturn@gmail.com>
2014-12-03 13:58:23 -0500

    database created

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,10 +3,10 @@ class WelcomeController < ApplicationController
   def index
     query = params['query'].gsub(' ', '+')
     url = "http://api.untappd.com/v4/search/beer?client_id=16E1853C36CF3E162699B468424A48C4279030EA&client_secret=B4BC7FB7D8CD6BDA886E92939B388004A3ACC89D&q=#{query}"
-    response = HTTParty.get(url)
-    beer = response['response']['beers']['items'].first['beer']
-    percent = beer['beer_abv']
-    beer = beer_name['beer_name']
+    @response = HTTParty.get(url)
+    beer = @response['response']['beers']['items'].first['beer']
+    @percent = beer['beer_abv']
+    @beer = beer_name['beer_name']
   end
 
 end
