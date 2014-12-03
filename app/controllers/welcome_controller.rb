797f29ae03edd17d5d46941887c739210ca79285
Evan <evturn@gmail.com>
2014-12-03 14:25:56 -0500

    back button added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,11 +5,9 @@ class WelcomeController < ApplicationController
   end
 
   def show
+    query = params['query'].gsub(' ', '+')
     url = "http://api.untappd.com/v4/search/beer?client_id=16E1853C36CF3E162699B468424A48C4279030EA&client_secret=B4BC7FB7D8CD6BDA886E92939B388004A3ACC89D&q=#{query}"
     response = HTTParty.get( url )
-    
-    query = params['query'].gsub(' ', '+')
-    
     beer = response['response']['beers']['items'].first['beer']
     @beer_abv = beer['beer_abv']
     @beer_name = beer['beer_name']
