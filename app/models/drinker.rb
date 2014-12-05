75990f540ac39e51d09e5235cf2ddb4a9984a84b
Evan <evturn@gmail.com>
2014-12-04 22:04:58 -0500

    built a calculator that is inaccurate

--- app/models/drinker.rb
+++ app/models/drinker.rb
@@ -10,3 +10,24 @@ class Drinker < ActiveRecord::Base
 
 
 end
+
+
+class Drinker
+
+  def initialize(fl_oz, hours, lbs)
+
+    @fl_oz = fl_oz
+    @hours = hours
+    @lbs = lbs
+
+  end 
+
+
+end
+
+# 80 kg male drinking 3 standard drinks in two hours:
+#  EBAC = (0.806 \cdot 3 \cdot 1.2)/(0.58 \cdot 80) - (0.015 \cdot 2) = 0.032534483 \approx 0.033 g/dL
+
+brad = Drinker.new( 12, 1, 140 )
+
+bac = (@fl_oz * 5.14/@lbs * 0.73) - 0.15 * @hours
--- app/models/round.rb
+++ app/models/round.rb
@@ -3,21 +3,52 @@ belongs_to :drinker
       
   def initialize(drinker)
 
-    @fl_oz = fl_oz
-    @hours = hours
+
+    @fl_oz = fl_oz['params']
+    @hours = hours['params']
+    Round.update({ fl_oz: @fl_oz, hours: @hours })
+
+
 
   end
 
-  def calculate
+  def calculate(abv, lbs, sex, hours)
 
-    m(fl_oz * 5.14/lbs * 0.73) - 0.15 * hours
-    f(fl_oz * 5.14/lbs * 0.66) - 0.15 * hours
     
-    # fl_oz         - total liquid ounces consumed * 5.14 (ounces of the total consumed that are just alchol)
-    # 5.14          - a conversion factor of .823 * 100/16, wherein .823 is used to convert fl_oz to ounces of lbs, 100 is used to convert the final to percentage and 16 is used to convert lbs to ounces 
-    # 0.73 and 0.66 - alcohol distribution ratios
-    # .015          - is the average alcohol elimination rate.
+    r = sex == "male" ? 0.73 : 0.66
+    bac = ((abv * 5.14) / (lbs * r)) - (0.015 * hours)
+
   end
 
 
 end
+    step_1 = (fl_oz * abv) # Multiply Ounces by Alcohol Volume
+    step_2 = (step_1 * 100 * 1.055) # Multiply by Blood Gravity
+    step_3 = (lbs * sex) # Multiply by Weigh and Sex
+    step_4 = (step_2 / step_3) # Divide Blood by Person Stats
+    step_5 = (hours * 0.015)  # Multiply Hours by Elimination Constant
+    step_6 = (step_4 - step_5) # Subtract by Hours and Elimination Constant
+    step_7 = (step_6 / 100)
+
+    
+    step_1 = (fl_oz * 5.14)
+    step_2 = (lbs * sex)
+    step_3 = (step_1 / step_2)
+    step_4 = (0.015 * hours)
+    step_5 = (step_3 - step_4)
+    step_6 = (step_5 / 100)
+
+
+    0.045
+    0.050
+           * 100 * 1.055 / lbs * sex) - (0.015 * hours)
+
+    bac = (12 * 0.06 * 100 * 1.055 / 140 * 0.68) - (0.015 * hours)
+
+     (fl_oz * 5.14/lbs * 0.73) - 0.15 * hours
+     (fl_oz * 5.14/lbs * 0.66) - 0.15 * hours
+
+    # fl_oz         - total liquid ounces consumed * 5.14 (ounces of the total consumed that are just alchol)
+    # 5.14          - a conversion factor of .823 * 100/16, wherein .823 is used to convert fl_oz to ounces of lbs, 100 is used to convert the final to percentage and 16 is used to convert lbs to ounces 
+    # 0.73 and 0.66 - alcohol distribution ratios
+    # .015          - is the average alcohol elimination rate.
